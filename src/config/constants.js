3666f1a3018f7e9251d8d258ad53c59142542768
addison912 <addisondrewmoore@gmail.com>
2019-12-15 01:21:12 -0800

    switched to DO backend server

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,6 @@
 module.exports = {
-  domain: "http://localhost:3001",
+  domain: "http://157.245.165.76:3001",
+  // domain: "http://localhost:3001",
   categories: [
     "Still Life",
     "Portraits",
