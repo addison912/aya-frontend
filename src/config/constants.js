030c8a65f935068b79b5669f92d70009cf91a828
addison912 <addisondrewmoore@gmail.com>
2019-12-17 19:22:11 -0800

    about to switch grid layout

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,7 +1,8 @@
 module.exports = {
   // domain: "http://157.245.165.76:3001",
-  domain: "http://157.245.165.76",
+  // domain: "http://157.245.165.76",
   // domain: "http://localhost:3001",
+  domain: "http://funky.haus",
   categories: [
     "Still Life",
     "Portraits",
