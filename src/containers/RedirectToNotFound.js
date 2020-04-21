ce04579c4cddb8d18f89993379449c1d6c06db44
Addison <addisondrewmoore@gmail.com>
2020-04-20 18:19:27 -0700

    redirect page styling

--- src/containers/RedirectToNotFound.js
+++ src/containers/RedirectToNotFound.js
@@ -5,14 +5,25 @@ import App from "../App";
 
 class RedirectToNotFound extends Component {
   componentDidMount() {
-    console.log(this.props);
     navigate(`/#/404`);
     window.location.reload();
   }
   render() {
     return (
-      <div className="pageContainer redirect">
-        <h1>Page not found. Redirecting your request.</h1>
+      <div className="page-container redirect">
+        <div className="loading-wrapper">
+          <div className="loading-container">
+            <h1 className="loading">
+              Page not found. Redirecting your request
+            </h1>
+          </div>
+          <div className="ellipsis-container">
+            <h1 className="ellipsis-wrapper">
+              {" "}
+              <span className="ellipsis"></span>
+            </h1>
+          </div>
+        </div>
       </div>
     );
   }
