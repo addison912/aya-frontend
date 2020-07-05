de435d247e1e7b5998a6abbe50739c02bd944599
Addison Moore <addisondrewmoore@gmail.com>
2020-07-05 03:08:22 -0700

    shop items populating from Square

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -7,35 +7,54 @@ class ShopItem extends Component {
         className={
           (this.props.item.availability &&
             this.props.item.availability == "0") ||
-          this.props.item.availability == "sold out"
+          this.props.item.availability == "sold out" ||
+          this.props.item.item_data.ecom_available == false
             ? "shop-item sold-out"
             : "shop-item"
         }
       >
         {this.props.item.photos[0] && this.props.item.photos.length == 1 ? (
-          <img src={this.props.item.photos[0]} alt={this.props.item.name} />
+          <img
+            src={this.props.item.photos[0].url}
+            alt={this.props.item.item_data.name}
+          />
         ) : (
           <h2>No image available</h2>
         )}
-        <h1>{this.props.item.name}</h1>
-        {this.props.item.price ? <p>${this.props.item.price}</p> : null}
-        {this.props.item.description ? (
-          <p>{this.props.item.description}</p>
+        <h1>{this.props.item.item_data.name}</h1>
+        {this.props.item.item_data.variations[0].item_variation_data.price_money
+          .amount &&
+        Number.isInteger(
+          this.props.item.item_data.variations[0].item_variation_data
+            .price_money.amount
+        ) ? (
+          <p>
+            $
+            {(
+              this.props.item.item_data.variations[0].item_variation_data
+                .price_money.amount / 100
+            ).toFixed(2)}
+          </p>
         ) : null}
-        {this.props.item.availability &&
+        {this.props.item.item_data.description ? (
+          <p>{this.props.item.item_data.description}</p>
+        ) : null}
+        {/* {this.props.item.item_data.availability &&
         !isNaN(parseInt(this.props.item.availability)) &&
         parseInt(this.props.item.availability) > 0 ? (
           <p>{this.props.item.availability} remaining</p>
-        ) : null}
+        ) : null} */}
         {(this.props.item.availability &&
           this.props.item.availability == "0") ||
-        this.props.item.availability == "sold out" ? (
+        this.props.item.availability == "sold out" ||
+        this.props.item.item_data.ecom_available == false ? (
           <button className="checkout-link">Sold Out</button>
-        ) : (this.props.item.availability &&
+        ) : this.props.item.item_data.ecom_available ||
+          (this.props.item.availability &&
             !isNaN(parseInt(this.props.item.availability)) &&
             parseInt(this.props.item.availability) > 0) ||
           this.props.item.availability == "unlimited" ? (
-          <a href="/">
+          <a href={`https://checkout.square.site/buy/${this.props.item.id}`}>
             <button className="checkout-link">Buy Now</button>
           </a>
         ) : null}
