233a409e6edb53e038a4bc948158d85f4d0003db
Addison Moore <addisondrewmoore@gmail.com>
2024-03-16 18:29:21 -0700

    updated packages

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,9 +1,9 @@
 module.exports = {
   // domain: "http://157.245.165.76",
   // domain: "http://64.227.97.131",
-  // domain: "http://localhost:3001",
+  domain: "http://localhost:8581",
   // domain: "https://admin.ayabrackett.com",
-  domain: "https://test-admin.ayabrackett.com",
+  // domain: "https://test-admin.ayabrackett.com",
 
   categories: [
     "Still Life",
@@ -11,6 +11,6 @@ module.exports = {
     "Travel",
     "Magazines",
     "Books",
-    "Advertising"
-  ]
+    "Advertising",
+  ],
 };
