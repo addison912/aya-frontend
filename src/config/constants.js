6638c373f6f35413104be29f94a2f40b25b2f185
Addison Moore <addisondrewmoore@gmail.com>
2024-03-19 21:26:07 -0700

    removed constants from gitignore

---
+++ src/config/constants.js
@@ -0,0 +1,14 @@
+module.exports = {
+  domain: "http://localhost:8581",
+  // domain: "https://admin.ayabrackett.com",
+  // domain: "https://test-admin.ayabrackett.com",
+
+  categories: [
+    "Still Life",
+    "Portraits",
+    "Travel",
+    "Magazines",
+    "Books",
+    "Advertising",
+  ],
+};
